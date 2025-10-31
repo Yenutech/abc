9b0602baf1331947acd6d55ca60ae2d689e7941e
Yenutech <sbudgetshop@gmail.com>
2025-10-31 13:19:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,5 +7,5 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=b2c00aafdd1bdddb' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+SESSION_NAME: process.env.PORT === undefined ? "yenusha" : process.env.SESSION_NAME,
 };
